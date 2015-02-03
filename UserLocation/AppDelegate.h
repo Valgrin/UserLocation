b11c00bb80aed2e51655f9dbb9ec12d17e939805
Thomas LEVY <thomas.levy@mappy.com>
2015-02-03 13:50:16 +0100

    Initial Commit

---
+++ UserLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserLocation
+//
+//  Created by Thomas LEVY on 03/02/15.
+//  Copyright (c) 2015 Thomas LEVY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UserLocation
+//
+//  Created by Thomas LEVY on 03/02/15.
+//  Copyright (c) 2015 Thomas LEVY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
